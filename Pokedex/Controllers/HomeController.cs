36717b6f61a6f2bef10ed78ee88ecc9080391344
Nicolas-Patrick <nicolasfadoni6@gmail.com>
2025-09-09 22:32:59 -0300

    Adicionado novas funções

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Pokedex.Data;
 using Pokedex.Models;
+using Pokedex.ViewModels;
 
 namespace Pokedex.Controllers;
 
@@ -26,6 +27,30 @@ public class HomeController : Controller
         .ThenInclude(t => t.Tipo)
         .ToList();
         return View(pokemons);
+    }
+
+    public IActionResult Details(int id)
+    {
+        Pokemon pokemon = _db.Pokemons
+            .Where(p => p.Numero == id)
+            .Include(p => p.Regiao)
+            .Include(p => p.Genero)
+            .Include(p => p.Tipos)
+            .ThenInclude(t => t.Tipo)
+            .SingleOrDefault();
+
+        DetailVM detail = new()
+        {
+            Atual = pokemon,
+            Anterior = _db.Pokemons
+              .OrderByDescending(p => p.Numero)
+              .FirstOrDefault(p => p.Numero < id),
+            Proximo = _db.Pokemons
+              .OrderBy(p => p.Numero)
+              .FirstOrDefault(p => p.Numero > id)
+        };
+
+        return View(detail);
     }
 
     public IActionResult Privacy()
